f3a73d07931c58721cc58ad0a3aa40e9bc2fee86
ManojaKumarB <ManojaKumarB@LP2-AP-51778528.PROD.HCLPNP.COM>
2022-12-20 17:11:38 +0530

    version 1

--- ProjectFirst/src/package1/Test.java
+++ ProjectFirst/src/package1/Test.java
@@ -3,7 +3,12 @@ package package1;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello!!!");
+		System.out.println("Hello!!!" + getName());
+
+	}
+	
+	private static String getName() {
+		return "Jack";
 
 	}
 
